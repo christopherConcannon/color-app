b12d93ea221ed7ee2879cc990e4326c4cd1645be
xtophe xanon <xocon10@gmail.com>
2020-05-11 14:50:40 -0500

    Make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -21,11 +21,11 @@ export default {
 		// }
 		[sizes.down('lg')]: {
 			width  : '25%',
-			height : (props) => (props.showingFullPalette ? '20%' : '50%')
+			height : (props) => (props.showingFullPalette ? '20%' : '33.3333%')
 		},
 		[sizes.down('md')]: {
 			width  : '50%',
-			height : (props) => (props.showingFullPalette ? '10%' : '50%')
+			height : (props) => (props.showingFullPalette ? '10%' : '20%')
 		},
 		[sizes.down('xs')]: {
 			width  : '100%',
@@ -116,15 +116,18 @@ export default {
 		opacity        : '0',
 		color          : 'white',
 		'& h1'         : {
-			fontWeight    : '400',
-			textShadow    : '1px 2px black',
-			background    : 'rgba(255, 255, 255, 0.2)',
-			width         : '100%',
-			textAlign     : 'center',
-			marginBottom  : '0',
-			padding       : '1rem',
-			textTransform : 'uppercase',
-			'& p'         : {
+			fontWeight         : '400',
+			textShadow         : '1px 2px black',
+			background         : 'rgba(255, 255, 255, 0.2)',
+			width              : '100%',
+			textAlign          : 'center',
+			marginBottom       : '0',
+			padding            : '1rem',
+			textTransform      : 'uppercase',
+			[sizes.down('xs')]: {
+				fontSize : '6rem'
+			},
+			'& p'              : {
 				fontSize   : '2rem',
 				fontWeight : '100'
 			}
--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes'
+
 export default {
 	Palette : {
 		height        : '100vh',
@@ -9,16 +11,16 @@ export default {
 		height : '90%'
 	},
 	goBack  : {
-		width           : '20%',
-		height          : (props) => (props.showingFullPalette ? '25%' : '50%'),
-		margin          : '0 auto',
-		display         : 'inline-block',
-		position        : 'relative',
-		cursor          : 'pointer',
-		marginBottom    : '-3.5px',
-		opacity         : 1,
-		backgroundColor : 'black',
-		'& a'           : {
+		width              : '20%',
+		height             : '50%',
+		margin             : '0 auto',
+		display            : 'inline-block',
+		position           : 'relative',
+		cursor             : 'pointer',
+		marginBottom       : '-3.5px',
+		opacity            : 1,
+		backgroundColor    : 'black',
+		'& a'              : {
 			color          : 'white',
 			width          : '100px',
 			height         : '30px',
@@ -37,6 +39,18 @@ export default {
 			textTransform  : 'uppercase',
 			border         : 'none',
 			textDecoration : 'none'
+		},
+		[sizes.down('lg')]: {
+			width  : '25%',
+			height : '33.3333%'
+		},
+		[sizes.down('md')]: {
+			width  : '50%',
+			height : '20%'
+		},
+		[sizes.down('xs')]: {
+			width  : '100%',
+			height : '10%'
 		}
 	}
 }
