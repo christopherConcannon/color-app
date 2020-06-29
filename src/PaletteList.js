58a31a3d439a5eab8dc69b8964840d6739a7e2fe
xtophe xanon <xocon10@gmail.com>
2020-06-29 12:14:54 -0500

    changed title

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -52,7 +52,7 @@ export class PaletteList extends Component {
 			<div className={classes.root}>
 				<div className={classes.container}>
 					<nav className={classes.nav}>
-						<h1 className={classes.heading}>React Colors</h1>
+						<h1 className={classes.heading}>Color Palette Customizer</h1>
 						<Link to="/palette/new">Create Palette</Link>
 					</nav>
 					<TransitionGroup className={classes.palettes}>
